65ceeba05e389c2d336c9b8239a750b13783259f
Nick DeNardis <nick.denardis@gmail.com>
2020-07-06 06:47:13 -0400

    Google Analytics added

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,37 +1,46 @@
 module.exports = {
-  title: 'Nick DeNardis',
+  title: "Nick DeNardis",
   plugins: [
-    ['@vuepress/blog', {
-      directories: [
-        {
-          id: 'post',
-          dirname: '_posts',
-          path: '/',
-          itemPermalink: '/:year/:month/:day/:slug',
-          pagination: {
-            perPagePosts: 10,
+    [
+      "@vuepress/blog",
+      {
+        directories: [
+          {
+            id: "post",
+            dirname: "_posts",
+            path: "/",
+            itemPermalink: "/:year/:month/:day/:slug",
+            pagination: {
+              perPagePosts: 10,
+            },
           },
+        ],
+        frontmatters: [
+          {
+            id: "tag",
+            keys: ["tag", "tags"],
+            path: "/tag/",
+            layout: "Tag",
+            frontmatter: { title: "Tag" },
+            itemlayout: "Tag",
+            pagination: {
+              perPagePosts: 3,
+            },
+          },
+        ],
+        sitemap: {
+          hostname: "http://localhost:8080",
         },
-      ],
-      frontmatters: [
-        {
-          id: "tag",
-          keys: ['tag', 'tags'],
-          path: '/tag/',
-          layout: 'Tag',
-          frontmatter: { title: 'Tag' },
-          itemlayout: 'Tag',
-          pagination: {
-            perPagePosts: 3
-          }
+        feed: {
+          canonical_base: "http://localhost:8080",
         },
-      ],
-      sitemap: {
-        hostname: "http://localhost:8080",
       },
-      feed: {
-        canonical_base: "http://localhost:8080",
-      }
-    }],
+    ],
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "UA-2439165-1",
+      },
+    ],
   ],
-}
+};
